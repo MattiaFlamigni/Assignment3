908c12ba08697574e65fd2e14c3df890c807f163
Mattia Flamigni <mattia.flamigni4@studio.unibo.it>
2024-01-22 17:34:57 +0100

    test main functionality for arduino

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,18 @@
 #include <Arduino.h> 
-void setup(){
+#include "components/Led.h"
 
+
+
+Led* led;
+
+void setup(){
+    led = new Led(12); 
 }
 
 
 void loop(){
-
+  led->switchOn();
+  delay(1000);
+  led->switchOff();  
+  delay(1000);
 }
