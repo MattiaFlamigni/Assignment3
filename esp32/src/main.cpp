d9e12b943ebc39c6b735c84e15de19c860e4bb47
Mattia Flamigni <mattia.flamigni4@studio.unibo.it>
2024-01-27 00:15:13 +0100

    fixed problems in submitting message to broker

--- esp32/src/main.cpp
+++ esp32/src/main.cpp
@@ -1,62 +1,26 @@
-#include "MqttTask.h"
-#include "components/Sonar.h"
+#include <Arduino.h>
+#include "mqtt_client.h"
 #include "config.h"
-#include "components/Led.h" 
+#include "components/Sonar.h"
 
-const char *ssid = "Wind3 HUB-E99111";
-const char *password = "29190210";
-const char *mqtt_server = "broker.mqtt-dashboard.com";
-const int mqtt_port = 1883;
-
+// Setup MQTT client
+const char* ssid = "Wind3 HUB-E99111";
+const char* password = "29190210";
+const char* mqtt_server = "broker.mqtt-dashboard.com";
+const char* topic = "WaterLevel";
 Sonar* sonar;
-MqttTask mqttTask("WaterLevel", "Hello, Java Application!");
-Led* greenLed;
-Led* redLed;
+MQTT_Client mqtt_client(ssid, password, mqtt_server, topic);
 void setup() {
-    greenLed = new Led(GREEN_LED_PIN);
-    redLed = new Led(RED_LED_PIN);
-    
+    mqtt_client.setup();
 
-  Serial.begin(115200);
-  sonar = new Sonar(TRIG_PIN, ECHO_PIN, SONAR_TIME);
-
-  // Connessione WiFi
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.println("Connessione WiFi...");
-
-    greenLed->switchOff();
-    redLed->switchOn();
-
-  }
-    
-
-  // Inizializza la classe MqttTask
-  
-  mqttTask.begin();
+    /*Serial.begin(9600);
+    sonar = new Sonar(ECHO_PIN, TRIG_PIN, SONAR_TIME);*/
 }
 
 void loop() {
-  if (WiFi.status() == WL_CONNECTED) {
-    greenLed->switchOn();
-    redLed->switchOff();
-    // Puoi inviare dati al broker MQTT qui
-    // Ad esempio, puoi ottenere il valore dal sensore di livello d'acqua (sonar) e inviarlo al broker MQTT
-    float waterLevel = sonar->getDistance();
-    String message = "Water level: " + String(waterLevel);
-    Serial.println(message);
+    mqtt_client.loop();
 
-    //invia il messaggio al broker MQTT
-    //MqttTask mqttTask("WaterLevel", message.c_str()); //TODO controllare se funziona   
+    /*Serial.println(String("Distance: ") + sonar->getDistance());
+    delay(1000);*/
 
-    mqttTask.publish("WaterLevel", message.c_str()); 
-
-    delay(1000);
-
-  } else {
-    greenLed->switchOff();
-    redLed->switchOn();
-    Serial.println("Connessione WiFi persa");
-  }
 }
