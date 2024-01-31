ca40062e472c2839157400d8054603d4b84c38ac
Mattia Flamigni <mattia.flamigni4@studio.unibo.it>
2024-01-31 14:45:14 +0100

    changed topic for frequency

--- RiverMonitoringService/app/src/main/java/rivermonitoringservice/App.java
+++ RiverMonitoringService/app/src/main/java/rivermonitoringservice/App.java
@@ -9,7 +9,7 @@ public class App {
     public static void main(String[] args) {
         try {
             mqttSubscriber = new MqttSubscriber("tcp://broker.mqtt-dashboard.com:1883", "JavaSubscriber", "WaterLevel"   );
-            mqttPubblisher = new MqttPubblisher("tcp://broker.mqtt-dashboard.com:1883", "JavaPublisher", "ValveOpeningLevel");
+            mqttPubblisher = new MqttPubblisher("tcp://broker.mqtt-dashboard.com:1883", "JavaPublisher", "Frequency");
             mqttSubscriber.start();
         } catch (Exception e) {
             e.printStackTrace();
