d37d2f6450922161f06d42b8452c1792bb7665d4
= <filippo.badioli@studio.unibo.it>
2024-02-02 12:08:33 +0100

    added auto refresh of graph

--- Dashboard/app/src/main/java/dashboard/Dashboard.java
+++ Dashboard/app/src/main/java/dashboard/Dashboard.java
@@ -1,28 +1,43 @@
 package dashboard;
 
-import javax.swing.JFrame;
-import javax.swing.SwingUtilities;
+
+import javax.swing.*;
+
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
 import org.jfree.data.category.DefaultCategoryDataset;
+import java.awt.event.*;
 
 public class Dashboard extends JFrame {
 
+    int time;
+
     public Dashboard() {
         DefaultCategoryDataset dataset = new DefaultCategoryDataset();
-        dataset.addValue(1.0, "Serie1", "Categoria1");
-        dataset.addValue(4.0, "Serie1", "Categoria2");
-        dataset.addValue(3.0, "Serie1", "Categoria3");
-
-        //JFreeChart chart = ChartFactory.createBarChart("Titolo Grafico", "Asse X", "Asse Y", dataset);
-        JFreeChart chart = ChartFactory.createLineChart("Titolo Grafico", "Asse X", "Asse Y", dataset);
-        
+        dataset.addValue(1.0, "Water Level", time + "");
+        JFreeChart chart = ChartFactory.createLineChart("Livello acqua", "Tempo (secondi)", "Altezza", dataset);     
         ChartPanel chartPanel = new ChartPanel(chart);
         add(chartPanel);
+
+        ActionListener taskPerformer = new ActionListener() { // Aggiorna ogni 5 secondi
+            public void actionPerformed(ActionEvent e) {
+
+                if(dataset.getColumnCount() >= 10) {
+                    Comparable<?> category = dataset.getColumnKey(0);
+                    dataset.removeValue("Water Level", category);
+                }
+                dataset.addValue(time*0.23, "Water Level", time + "");
+                chart.fireChartChanged();
+                time++;
+            }
+        };
+        Timer timer = new Timer(1000, taskPerformer);
+        timer.start();
+
         
-        setTitle("Grafico con Java");
-        setSize(800, 600);
+        setTitle("Dashboard");
+        setSize(1000, 800);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
