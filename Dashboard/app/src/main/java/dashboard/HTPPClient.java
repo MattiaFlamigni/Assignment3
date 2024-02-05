8f7200acd459957d453581011d5d51d85f6fb30a
Mattia Flamigni <mattia.flamigni4@studio.unibo.it>
2024-02-05 10:22:37 +0100

    fixed exception

--- Dashboard/app/src/main/java/dashboard/HTPPClient.java
+++ Dashboard/app/src/main/java/dashboard/HTPPClient.java
@@ -15,17 +15,21 @@ public class HTPPClient {
         con.setRequestMethod("GET");
     }
 
-    public static String getResponse() throws Exception{
-
-        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-        String inputLine;
-        StringBuffer response = new StringBuffer();
-
-        while ((inputLine = in.readLine()) != null) {
-            response.append(inputLine);
+    public static String getResponse() throws Exception {
+        URL url = new URL("http://localhost:8000/endpoint");
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("GET");
+    
+        try (BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()))) {
+            StringBuilder response = new StringBuilder();
+            String inputLine;
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            return response.toString();
+        } finally {
+            con.disconnect(); // Chiudi la connessione
         }
-        in.close();
-
-        return response.toString();
     }
+    
 }
