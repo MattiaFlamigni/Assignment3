1b14ec4000643c0c149f23587cc31eca60f8f05a
Mattia Flamigni <mattia.flamigni4@studio.unibo.it>
2024-02-05 11:08:45 +0100

    minor improvements

--- Dashboard/app/src/main/java/dashboard/Dashboard.java
+++ Dashboard/app/src/main/java/dashboard/Dashboard.java
@@ -1,50 +1,40 @@
 package dashboard;
 
-
 import javax.swing.*;
-
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
 import org.jfree.data.category.DefaultCategoryDataset;
 import java.awt.event.*;
-import java.net.http.HttpClient;
 
 public class Dashboard extends JFrame {
 
-    int time;
+    private int time;
+    private DefaultCategoryDataset dataset;
 
     public Dashboard() throws Exception {
-        new HTPPClient();
-        DefaultCategoryDataset dataset = new DefaultCategoryDataset();
-        dataset.addValue(1.0, "Water Level", time + "");
+        dataset = new DefaultCategoryDataset();
         JFreeChart chart = ChartFactory.createLineChart("Livello acqua", "Tempo (secondi)", "Altezza", dataset);     
         ChartPanel chartPanel = new ChartPanel(chart);
         add(chartPanel);
 
         ActionListener taskPerformer = new ActionListener() { // Aggiorna ogni 5 secondi
-            public void actionPerformed(ActionEvent e){
-
-                if(dataset.getColumnCount() >= 10) {
-                    Comparable<?> category = dataset.getColumnKey(0);
-                    dataset.removeValue("Water Level", category);
-                }
+            public void actionPerformed(ActionEvent e) {
                 try {
-
-
-                    dataset.addValue(Integer.parseInt(HTPPClient.getResponse()), "Water Level", time + "");
-                } catch (Exception e1) {
-                    // TODO Auto-generated catch block
-                    e1.printStackTrace();
+                    String response = HTPPClient.getResponse();
+                    dataset.addValue(Integer.parseInt(response), "Water Level", Integer.toString(time));
+                    if (time > 10) {
+                        dataset.removeValue("Water Level", Integer.toString(time - 10));
+                    }
+                    time++;
+                } catch (Exception ex) {
+                    ex.printStackTrace();
                 }
-                chart.fireChartChanged();
-                time++;
             }
         };
         Timer timer = new Timer(1000, taskPerformer);
         timer.start();
 
-        
         setTitle("Dashboard");
         setSize(1000, 800);
         setLocationRelativeTo(null);
